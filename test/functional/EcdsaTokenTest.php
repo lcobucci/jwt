92440c99477662f3c20f03df434335d995245a0b
Paragon Initiative Enterprises <security@paragonie.com>
2015-12-31 17:13:07 -0500

    Add functional test to catch JWT issues:
    https://auth0.com/blog/2015/03/31/critical-vulnerabilities-in-json-web-token-libraries/

--- test/functional/EcdsaTokenTest.php
+++ test/functional/EcdsaTokenTest.php
@@ -13,6 +13,7 @@ use Lcobucci\JWT\Token;
 use Lcobucci\JWT\Signature;
 use Lcobucci\JWT\Signer\Ecdsa\Sha256;
 use Lcobucci\JWT\Signer\Ecdsa\Sha512;
+use Lcobucci\JWT\Signer\Hmac\Sha512 as HS512;
 use Lcobucci\JWT\Signer\Keychain;
 use Lcobucci\JWT\Keys;
 
@@ -245,5 +246,75 @@ class EcdsaTokenTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals('world', $token->getClaim('hello'));
         $this->assertTrue($token->verify(new Sha512(), $keychain->getPublicKey($key)));
+    }
+    
+    /**
+     * @test
+     *
+     * @covers Lcobucci\JWT\Builder
+     * @covers Lcobucci\JWT\Parser
+     * @covers Lcobucci\JWT\Token
+     * @covers Lcobucci\JWT\Signature
+     * @covers Lcobucci\JWT\Signer\Key
+     * @covers Lcobucci\JWT\Signer\BaseSigner
+     * @covers Lcobucci\JWT\Signer\Ecdsa
+     * @covers Lcobucci\JWT\Signer\Ecdsa\KeyParser
+     * @covers Lcobucci\JWT\Signer\Ecdsa\Sha512
+     * @covers Lcobucci\JWT\Signer\Hmac\Sha512
+     * @covers Lcobucci\JWT\Signer\Keychain
+     * @covers Lcobucci\JWT\Claim\Factory
+     * @covers Lcobucci\JWT\Claim\Basic
+     */
+    public function preventRegressionsThatAllowsMaliciousTampering()
+    {
+        $data = 'eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJoZWxsbyI6IndvcmxkIn0.'
+                . 'AQx1MqdTni6KuzfOoedg2-7NUiwe-b88SWbdmviz40GTwrM0Mybp1i1tVtm'
+                . 'TSQ91oEXGXBdtwsN6yalzP9J-sp2YATX_Tv4h-BednbdSvYxZsYnUoZ--ZU'
+                . 'dL10t7g8Yt3y9hdY_diOjIptcha6ajX8yzkDGYG42iSe3f5LywSuD6FO5c';
+
+        $key = '-----BEGIN PUBLIC KEY-----' . PHP_EOL
+               . 'MIGbMBAGByqGSM49AgEGBSuBBAAjA4GGAAQAcpkss6wI7PPlxj3t7A1RqMH3nvL4' . PHP_EOL
+               . 'L5Tzxze/XeeYZnHqxiX+gle70DlGRMqqOq+PJ6RYX7vK0PJFdiAIXlyPQq0B3KaU' . PHP_EOL
+               . 'e86IvFeQSFrJdCc0K8NfiH2G1loIk3fiR+YLqlXk6FAeKtpXJKxR1pCQCAM+vBCs' . PHP_EOL
+               . 'mZudf1zCUZ8/4eodlHU=' . PHP_EOL
+               . '-----END PUBLIC KEY-----';
+        
+        $dec = new \Lcobucci\Jose\Parsing\Parser;
+        
+        /**
+         * Let's let the attacker tamper with our message!
+         * 
+         * @ref https://auth0.com/blog/2015/03/31/critical-vulnerabilities-in-json-web-token-libraries/
+         */
+        $asplode = explode('.', $data);
+        
+        // The user is lying; we insist that we're using HMAC-SHA512, with the
+        // public key as the HMAC secret key. This just builds a forged message:
+        $asplode[0] = $dec->base64UrlEncode('{"alg":"HS512","typ":"JWT"}');
+        $hmac = hash_hmac(
+            'sha512',
+            $asplode[0] . '.' . $asplode[1],
+            $key,
+            true
+        );
+        $asplode[2] = $dec->base64UrlEncode($hmac);
+        $bad = implode('.', $asplode);
+        
+        /**
+         * At this point, we have our forged message in $bad for testing...
+         * 
+         * Now, if we allow the attacker to dictate what Signer we use
+         * (e.g. HMAC-SHA512 instead of ECDSA), they can forge messages!
+         */
+        $keychain = new Keychain();
+        $token = (new Parser())->parse((string) $bad);
+        $this->assertEquals('world', $token->getClaim('hello'));
+        // Note we're using HS512() here:
+        $this->assertTrue($token->verify(new HS512(), $keychain->getPublicKey($key)));
+        
+        /**
+         * Fortunately, we're passing a separate Signer, so this fails:
+         */
+        $this->assertFalse($token->verify(new Sha512(), $keychain->getPublicKey($key)));
     }
 }
