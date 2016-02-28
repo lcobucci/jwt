06a3c666e61b0ec8c5c1da8e43ba196808c613cf
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-02-28 16:51:16 +0000

    Create tests for ContainsEqualsTo

---
+++ test/unit/Claim/ContainsEqualsToTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * This file is part of Lcobucci\JWT, a simple library to handle JWT and JWS
+ *
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ */
+
+declare(strict_types=1);
+
+namespace Lcobucci\JWT\Claim;
+
+use Lcobucci\JWT\ValidationData;
+
+/**
+ * @author Matthew John Marshall <matthew.marshall96@yahoo.co.uk>
+ * @since 4.x.x
+ */
+class ContainsEqualsToTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     *
+     * @uses Lcobucci\JWT\Claim\Basic::__construct
+     * @uses Lcobucci\JWT\Claim\Basic::getName
+     * @uses Lcobucci\JWT\ValidationData::__construct
+     * @uses Lcobucci\JWT\ValidationData::has
+     *
+     * @covers Lcobucci\JWT\Claim\ContainsEqualsTo::validate
+     */
+    public function validateShouldReturnTrueWhenValidationDoesntHaveTheClaim()
+    {
+        $claim = new ContainsEqualsTo('aud', ['test', 'test2']);
+
+        $this->assertTrue($claim->validate(new ValidationData()));
+    }
+
+    /**
+     * @test
+     *
+     * @uses Lcobucci\JWT\Claim\Basic::__construct
+     * @uses Lcobucci\JWT\Claim\Basic::getName
+     * @uses Lcobucci\JWT\Claim\Basic::getValue
+     * @uses Lcobucci\JWT\ValidationData::__construct
+     * @uses Lcobucci\JWT\ValidationData::setIssuer
+     * @uses Lcobucci\JWT\ValidationData::has
+     * @uses Lcobucci\JWT\ValidationData::get
+     *
+     * @covers Lcobucci\JWT\Claim\ContainsEqualsTo::validate
+     */
+    public function validateShouldReturnTrueWhenValidationDataValueIsContained()
+    {
+        $claim = new ContainsEqualsTo('aud', ['test', 'test2']);
+
+        $data = new ValidationData();
+        $data->setAudience('test');
+
+        $this->assertTrue($claim->validate($data));
+    }
+
+    /**
+     * @test
+     *
+     * @uses Lcobucci\JWT\Claim\Basic::__construct
+     * @uses Lcobucci\JWT\Claim\Basic::getName
+     * @uses Lcobucci\JWT\Claim\Basic::getValue
+     * @uses Lcobucci\JWT\ValidationData::__construct
+     * @uses Lcobucci\JWT\ValidationData::setIssuer
+     * @uses Lcobucci\JWT\ValidationData::has
+     * @uses Lcobucci\JWT\ValidationData::get
+     *
+     * @covers Lcobucci\JWT\Claim\ContainsEqualsTo::validate
+     */
+    public function validateShouldReturnFalseWhenValidationDataValueIsNotContained()
+    {
+        $claim = new EqualsTo('aud', ['test', 'test2']);
+
+        $data = new ValidationData();
+        $data->setAudience('test3');
+
+        $this->assertFalse($claim->validate($data));
+    }
+}
