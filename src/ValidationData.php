f8b2771b29b3acb61ea9c6b2ee2330607742b73f
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-02-28 17:51:11 +0000

    Allow array of strings for 'iss' validation (as implied in RFC7519-4.1.7)

--- src/ValidationData.php
+++ src/ValidationData.php
@@ -55,11 +55,18 @@ class ValidationData
     /**
      * Configures the issuer
      *
-     * @param string $issuer
+     * @param string|array $issuer
      */
     public function setIssuer($issuer)
     {
-        $this->items['iss'] = (string) $issuer;
+        if (is_array($issuer)) {
+            foreach($issuer as $key => $member) {
+                $issuer[$key] = (string) $member;
+            }
+            $this->items['iss'] = $issuer;
+        } else {
+            $this->items['iss'] = [(string) $issuer];
+        }
     }
 
     /**
