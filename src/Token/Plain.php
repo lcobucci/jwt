d24671193d1a820ba0b3794cc80c4cfc7071ce5a
fyrye <fyrye@users.noreply.github.com>
2021-02-11 18:49:03 -0500

    Support composer classmap-authoritative option
    Fix #666
    Forcibly calls `class_alias()` when `class_exists()` autoload feature fails due to composer `classmap-authoritative` option being enabled.

--- src/Token/Plain.php
+++ src/Token/Plain.php
@@ -5,4 +5,4 @@ namespace Lcobucci\JWT\Token;
 use Lcobucci\JWT\Token;
 use function class_alias;
 
-class_alias(Token::class, Plain::class);
+class_exists(Plain::class, false) || class_alias(Token::class, Plain::class);
