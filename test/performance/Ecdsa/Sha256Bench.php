c7ada979caff9d4075441ee2641fe83d30d6b031
Luís Cobucci <lcobucci@gmail.com>
2022-08-17 23:36:15 +0200

    Use correct ECDSA key size for each algorithm
    Signed-off-by: Luís Cobucci <lcobucci@gmail.com>

--- test/performance/Ecdsa/Sha256Bench.php
+++ test/performance/Ecdsa/Sha256Bench.php
@@ -5,11 +5,25 @@ namespace Lcobucci\JWT\Ecdsa;
 
 use Lcobucci\JWT\Signer;
 use Lcobucci\JWT\Signer\Ecdsa\Sha256;
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\SignerBench;
+use PhpBench\Benchmark\Metadata\Annotations\Groups;
 
-final class Sha256Bench extends EcdsaBench
+/** @Groups({"ECDSA"}) */
+final class Sha256Bench extends SignerBench
 {
     protected function signer(): Signer
     {
         return Sha256::create();
+    }
+
+    protected function signingKey(): Key
+    {
+        return Key\InMemory::file(__DIR__ . '/private-256.key');
+    }
+
+    protected function verificationKey(): Key
+    {
+        return Key\InMemory::file(__DIR__ . '/public-256.key');
     }
 }
--- test/performance/Ecdsa/Sha384Bench.php
+++ test/performance/Ecdsa/Sha384Bench.php
@@ -5,11 +5,25 @@ namespace Lcobucci\JWT\Ecdsa;
 
 use Lcobucci\JWT\Signer;
 use Lcobucci\JWT\Signer\Ecdsa\Sha384;
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\SignerBench;
+use PhpBench\Benchmark\Metadata\Annotations\Groups;
 
-final class Sha384Bench extends EcdsaBench
+/** @Groups({"ECDSA"}) */
+final class Sha384Bench extends SignerBench
 {
     protected function signer(): Signer
     {
         return Sha384::create();
+    }
+
+    protected function signingKey(): Key
+    {
+        return Key\InMemory::file(__DIR__ . '/private-384.key');
+    }
+
+    protected function verificationKey(): Key
+    {
+        return Key\InMemory::file(__DIR__ . '/public-384.key');
     }
 }
