800dbdd145ace04beea6d66307ac43947ab64517
Luís Cobucci <lcobucci@gmail.com>
2023-11-19 23:01:37 +0100

    Introduce fake implementation of signer for tests
    This allows us to stop using PHPUnit mocks in every place that needs,
    making tests more obvious.
    
    Signed-off-by: Luís Cobucci <lcobucci@gmail.com>

--- tests/Validation/Constraint/ConstraintTestCase.php
+++ tests/Validation/Constraint/ConstraintTestCase.php
@@ -3,9 +3,14 @@ declare(strict_types=1);
 
 namespace Lcobucci\JWT\Tests\Validation\Constraint;
 
+use Closure;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\JwtFacade;
+use Lcobucci\JWT\Signer;
 use Lcobucci\JWT\Token\DataSet;
 use Lcobucci\JWT\Token\Plain;
 use Lcobucci\JWT\Token\Signature;
+use Lcobucci\JWT\UnencryptedToken;
 use PHPUnit\Framework\TestCase;
 
 abstract class ConstraintTestCase extends TestCase
@@ -23,6 +28,15 @@ abstract class ConstraintTestCase extends TestCase
             new DataSet($headers, ''),
             new DataSet($claims, ''),
             $signature ?? new Signature('sig+hash', 'sig+encoded'),
+        );
+    }
+
+    protected function issueToken(Signer $signer, Signer\Key $key, ?Closure $customization = null): UnencryptedToken
+    {
+        return (new JwtFacade())->issue(
+            $signer,
+            $key,
+            $customization ?? static fn (Builder $builder) => $builder,
         );
     }
 }
